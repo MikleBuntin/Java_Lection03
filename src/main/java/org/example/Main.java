aad1988f56fb4df27ddb7cbd7567568040858e15
MikleBuntin <BuntinMA@mail.ru>
2023-02-18 19:26:08 +0300

    Initial commit

---
+++ src/main/java/org/example/Main.java
@@ -0,0 +1,11 @@
+package org.example;
+
+public class Main {
+
+//    Здесь будет работа не семинаре
+
+
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+    }
+}
