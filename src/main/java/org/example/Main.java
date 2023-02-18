5bb54c0829cc4ce4fbf28bf10e34407848e42b10
MikleBuntin <BuntinMA@mail.ru>
2023-02-18 20:23:26 +0300

    First task

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,11 +1,46 @@
 package org.example;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Random;
+
+//import static jdk.internal.logger.DefaultLoggerFinder.SharedLoggers.system;
+//import static jdk.internal.org.jline.reader.impl.LineReaderImpl.CompletionType.List;
+
 public class Main {
-
-//    Здесь будет работа не семинаре
-
-
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+//        Scanner iScanner = new Scanner(System.in);
+////        System.out.printf("Привет! ");
+//        System.out.printf("Введте размер числового массива: ");
+//        String input_text = iScanner.nextLine();
+//        int arr_len = Integer.parseInt(input_text);
+//        iScanner.close();
+
+//        int[] array = new int[arr_len];
+
+//        for (int i = 0; i < array.length; i++) {
+//            array[i] = (int) (Math.random() * 100);
+//            System.out.println("Array[i]: " + array[i]);
+
+            List<Integer> list = new ArrayList<>();
+            FillList(list);
+            OutPutList(list);
+        }
+
+        public static void FillList(List<Integer> list){
+            Random random = new Random();
+            for (int i = 0; i < 10;  i++){
+                int rand = random.nextInt(100);
+                list.add(rand);
+            }
+        }
+        public static void OutPutList(List<Integer> list){
+            list.sort(null);
+            for (Integer integer : list) {
+                System.out.println(integer + ' ');
+            }
+            System.out.println();
+        }
+
+//        System.out.println("Hello world!");
     }
-}
