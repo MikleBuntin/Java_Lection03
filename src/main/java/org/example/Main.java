9a097794abc4f255bece956323f54b6b0eeb824a
MikleBuntin <BuntinMA@mail.ru>
2023-02-20 10:10:21 +0300

    Second task. Planets

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -3,6 +3,7 @@ package org.example;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Random;
+import java.util.Scanner;
 
 //import static jdk.internal.logger.DefaultLoggerFinder.SharedLoggers.system;
 //import static jdk.internal.org.jline.reader.impl.LineReaderImpl.CompletionType.List;
@@ -10,35 +11,31 @@ import java.util.Random;
 public class Main {
     public static void main(String[] args) {
 
-        int[] array = new int[arr_len]; // Созадаём пустой список нужного размера
+//        int[] resultArray = new int[arr_len]; // Созадаём пустой список нужного размера
 
-        //        Запрашиваем размер списка
-        Scanner iScanner = new Scanner(System.in);
-        System.out.printf("Привет! ");
-        System.out.printf("Введте размер числового массива: ");
-        String input_text = iScanner.nextLine();
-        int arr_len = Integer.parseInt(input_text);
-        iScanner.close();
+        String[] planets = {"Mercury", "Venera", "Earth", "Mars", "Jupyter", "Saturn", "Uran", "Neptun", "Pluton"};
 
-        int[] resultArray = new int[arr_len]; // Созадаём пустой список нужного размера
-
-
-        for (int i = 0; i < array.length; i++) { // Запоняем список значениями, взятыми из списка планет
-            array[i] = (int) (Math.random() * 100);
-            System.out.println("Array[i]: " + resultArray[i]);
-
-
-            List<String> planets = new ArrayList<>();
-            List<String> resultList = new ArrayList<>();
-//            FillList(list);
+            ArrayList<String> resultList = new ArrayList<>();
+            FillList(planets[]);
 //            OutPutList(list);
         }
 
-        public static void FillList(List<Integer> list){
+        public static void FillList(String[] planetList){
+
+//                    Запрашиваем размер списка
+            Scanner iScanner = new Scanner(System.in);
+            System.out.printf("Привет! ");
+            System.out.printf("Введте размер числового массива: ");
+            String input_text = iScanner.nextLine();
+            int arr_len = Integer.parseInt(input_text);
+            iScanner.close();
             Random random = new Random();
-            for (int i = 0; i < 10;  i++){
-                int rand = random.nextInt(100);
-                list.add(rand);
+
+            for (int i = 0; i < arr_len;  i++){
+                int rand = random.nextInt(planetList.toArray().length);
+                list.add(list.get(rand));
+
+
             }
         }
         public static void OutPutList(List<Integer> list){
